aa3bfd672e2526c89f7fb8e95e933dea38209204
joaowatchful <joao@watchful.ltd>
2023-10-14 22:21:20 +0100

    configured Transaction model

--- RestfulAPI/app/Models/Transaction.php
+++ RestfulAPI/app/Models/Transaction.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable =  [
+        'vcard',
+        'type',
+        'value',
+        'old_balance',
+        'new_balance',
+        'payment_type',
+        'payment_reference',
+        'pair_transaction',
+        'pair_vcard',
+        'category_id',
+        'custom_options',
+        'custom_data'
+    ];
+
+    protected $casts = [
+        'custom_options' => 'array',
+        'custom_data' => 'array'
+    ];
 }
