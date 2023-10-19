f3ea93d2b0a3d1d96210ea1c0c03b5705e4c80b5
joaowatchful <joao@watchful.ltd>
2023-10-20 00:05:50 +0100

    added show method in VCardController

--- RestfulAPI/app/Http/Controllers/VCardController.php
+++ RestfulAPI/app/Http/Controllers/VCardController.php
@@ -7,6 +7,7 @@ use App\Models\VCard;
 
 class VCardController extends Controller
 {
+    // esta função só deve ser chamada por um administrador (falta implementar a autorização)
     public function index()
     {
         $vCards = VCard::all();
@@ -14,6 +15,24 @@ class VCardController extends Controller
             'success' => true,
             'message' => 'Successfully retrieved vcards',
             'data' => $vCards
+        ], 200);
+    }
+
+    public function show($phoneNumber)
+    {
+        $vCard = VCard::find($phoneNumber);
+
+        if (!$vCard) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, vcard with phone number "' . $phoneNumber . '" cannot be found'
+            ], 400);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully retrieved vcard',
+            'data' => $vCard
         ], 200);
     }
 }
