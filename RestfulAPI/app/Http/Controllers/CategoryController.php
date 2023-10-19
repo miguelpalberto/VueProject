85adc00b427d00dcbab3a56d97423347f2884c42
joaowatchful <joao@watchful.ltd>
2023-10-19 18:51:20 +0100

    added CategoryController

---
+++ RestfulAPI/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $categories = Category::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully retrieved categories',
+            'data' => $categories
+        ], 200);
+    }
+}
