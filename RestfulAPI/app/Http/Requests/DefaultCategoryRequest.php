336dd6051971bb5ece5b2cba73122f5301c445ab
João Silva <2211429@my.ipleiria.pt>
2023-10-28 17:42:27 +0100

    cenas

--- RestfulAPI/app/Http/Requests/DefaultCategoryRequest.php
+++ RestfulAPI/app/Http/Requests/DefaultCategoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class DefaultCategoryRequest extends FormRequest
 {
@@ -23,7 +24,12 @@ class DefaultCategoryRequest extends FormRequest
     {
         return [
             'type' => 'required|in:C,D',
-            'name' => 'required|string|max:255',
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('name')->ignore($this->id),
+            ]
         ];
     }
 
@@ -34,6 +40,7 @@ class DefaultCategoryRequest extends FormRequest
             'name.max' => 'The maximum character limit for the name is 255 characters',
             'name.required' => 'The name is required',
             'name.string' => 'The name must be a string',
+            'name:unique' => 'The name must be unique',
             'type.required' => 'The transaction type is required',
             'type.in' => 'The type must be C (Credit) or D (Debit)',
         ];
