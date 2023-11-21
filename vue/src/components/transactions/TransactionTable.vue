e8308267f18498f0330171fd3814b132b5582811
Miguel Alberto <2211444@my.ipleiria.pt>
2023-11-21 23:50:27 +0000

    Transactions List

---
+++ vue/src/components/transactions/TransactionTable.vue
@@ -0,0 +1,183 @@
+<script setup>
+    import axios from 'axios'
+    import { ref, watch, watchEffect } from "vue";
+    
+    const props = defineProps({
+      transactions: {
+        type: Array,
+        default: () => [],
+    },
+      showId: {
+        type: Boolean,
+        default: true,
+    },
+  })
+    
+    const emit = defineEmits([
+      // "completeToggled",
+      // "edit",
+      // "deleted",
+    ]);
+
+
+    watch(
+      () => props.tasks,
+      (newTasks) => {
+        //editingTasks.value = newTasks;
+      }
+    );   
+    // Alternative to previous watch
+    // watchEffect(() => {
+    //   editingTasks.value = props.tasks
+    // })
+    
+    // const toogleClick = (task) => {
+    //   axios
+    //     .patch("tasks/" + task.id + "/completed", { completed: !task.completed })
+    //     .then((response) => {
+    //       task.completed = response.data.data.completed;
+    //       emit("completeToggled", task);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+    // };
+    // const editClick = (task) => {
+    //   emit("edit", task);
+    // };
+    // const deleteClick = (task) => {
+    //   axios
+    //     .delete("tasks/" + task.id)
+    //     .then((response) => {
+    //       let deletedTask = response.data.data;
+    //       emit("deleted", deletedTask);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+    // };
+    </script>
+    
+    <!-- <template>
+      <table class="table">
+        <thead>
+          <tr>
+            <th v-if="showId">#</th>
+            <th class="text-center" v-if="showCompleted">Completed</th>
+            <th>Description</th>
+            <th v-if="showOwner">Owner</th>
+            <th v-if="showProject">Project</th>
+            <th v-if="showCompletedButton || showEditButton || showDeleteButton"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="task in editingTasks" :key="task.id">
+            <td v-if="showId">{{ task.id }}</td>
+            <td class="text-center" v-if="showCompleted">
+              {{ task.completed ? "yes" : "-" }}
+            </td>
+            <td>
+              <span :class="{ completed: task.completed }">{{ task.description }}</span>
+            </td>
+            <td v-if="showOwner">{{ task.owner_name }}</td>
+            <td v-if="showProject">{{ task.project_name }}</td>
+            <td
+              class="text-end"
+              v-if="showCompletedButton || showEditButton || showDeleteButton"
+            >
+              <div class="d-flex justify-content-end">
+                <button
+                  class="btn btn-xs btn-light"
+                  @click="toogleClick(task)"
+                  v-if="showCompletedButton"
+                >
+                  <i
+                    class="bi bi-xs"
+                    :class="{
+                      'bi-square': !task.completed,
+                      'bi-check2-square': task.completed,
+                    }"
+                  ></i>
+                </button>
+    
+                <button
+                  class="btn btn-xs btn-light"
+                  @click="editClick(task)"
+                  v-if="showEditButton"
+                >
+                  <i class="bi bi-xs bi-pencil"></i>
+                </button>
+    
+                <button
+                  class="btn btn-xs btn-light"
+                  @click="deleteClick(task)"
+                  v-if="showDeleteButton"
+                >
+                  <i class="bi bi-xs bi-x-square-fill"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </template>
+     -->
+     <template>
+      <table class="table">
+        <thead>
+          <tr>
+            <th v-if="showId">#</th>
+            <th>From/To</th><!-- payment_reference column -->
+            <th>Type</th>
+            <th>Date</th>
+            <th>Value</th>
+            <th>Method</th>
+            <th>Category</th>
+            <th>Description</th>
+            <!-- <th>Time</th> -->
+            <!-- <th v-if="showEditButton || showDeleteButton"></th> -->
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="transaction in transactions"
+            :key="transaction.id"
+          >
+            <td v-if="showId">{{ transaction.id }}</td>
+            <td>{{ transaction.payment_reference }}</td>
+            <td>{{ transaction.type }}</td>
+            <td>{{ transaction.datetime }}</td>
+            <td>{{ transaction.value }}</td>
+            <td>{{ transaction.payment_type }}</td>
+            <td>{{ transaction.category_id }}</td>
+            <td>{{ transaction.description }}</td>
+            <!-- <td
+              class="text-end"
+              v-if="showEditButton || showDeleteButton"
+            >
+              <div class="d-flex justify-content-end">
+                <button
+                  class="btn btn-xs btn-light"
+                  @click="editClick(project)"
+                  v-if="showEditButton"
+                ><i class="bi bi-xs bi-pencil"></i>
+                </button>
+                </button>
+              </div>
+            </td> -->
+          </tr>
+        </tbody>
+      </table>
+    </template>
+
+    <style scoped>
+    .completed {
+      text-decoration: line-through;
+    }
+    
+    button {
+      margin-left: 3px;
+      margin-right: 3px;
+    }
+    </style>
+    
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 import axios from 'axios'
 import HomeView from '../views/HomeView.vue'
+import Transactions from "../components/transactions/Transactions.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,12 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: () => import('../components/auth/Login.vue')
-        }
+        },
+        {
+          path: "/transactions",
+          name: "transactions",
+          component: Transactions,
+        },
     ]
 })
 
