891c0583ade73776d917f5973e7bd2164acafb28
João Silva <2211429@my.ipleiria.pt>
2023-12-06 00:17:21 +0000

    Fix category constraint error

--- laravel/app/Http/Controllers/CategoryController.php
+++ laravel/app/Http/Controllers/CategoryController.php
@@ -21,6 +21,20 @@ class CategoryController extends Controller
     public function store(CategoryRequest $request){
         $validRequest = $request->validated();
 
+
+        //check if category combination exists
+        if (Category::where('vcard', $validRequest['vcard'])->where('type', $validRequest['type'])->where('name', $validRequest['name'])->exists()) {
+            $typeToPrettyString = $validRequest['type'] === 'C' ? 'Credit' : 'Debit';
+            return response()->json([
+                'errors' => [
+                    'name' => [
+                        "The category '" . $validRequest['name'] . "' with the type '" . $typeToPrettyString . "' already exists for this vCard."
+                    ]
+                ]
+            ], 422);
+        }
+
+
         $category = new Category();
         $category->vcard = $validRequest['vcard'];
         $category->type = $validRequest['type'];
