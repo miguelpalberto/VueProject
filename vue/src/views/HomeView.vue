509fdde1225efb3dfa9ce157957005280f360b8b
Miguel Alberto <2211444@my.ipleiria.pt>
2023-12-15 19:34:09 +0000

    Home Page

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,6 +2,23 @@
 </script>
 
 <template>
-  <main>
+  <main class="d-flex flex-column align-items-center justify-content-center">
+    <div class="mb-4">
+      <img src="@/assets/logogta1.svg" alt="" width="350" class="d-inline-block align-text-top" />
+    </div>
+    <br>
+    <router-link to="/login" class="btn btn-success px-5 mb-3">
+      <span role="login">Login</span>
+    </router-link>
+    <router-link to="/register" class="btn btn-success px-5">
+      <span role="login">Register</span>
+    </router-link>
   </main>
 </template>
+
+<style scoped>
+  main {
+    height: 70vh;
+  }
+</style>
+
